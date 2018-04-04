351d48d6f3c6d566ffa6e5086772d7540d3b14bb
catsuo <catsuo@tencent.com>
2018-04-04 10:30:37 +0800

    use threading

--- keepAliveTest/monkeyAndLogcat.py
+++ keepAliveTest/monkeyAndLogcat.py
@@ -1,12 +1,25 @@
 #coding=utf-8
+# a helper for logcat and monkey working together.
 
 import os
 import time
 import re
 import sys
 import io
+import threading
 
-# a helper for logcat and monkey working together.
+
+def runLogcat(device, logcatPath):
+	print 'runLogcat begin'
+	result = os.system('adb -s %s logcat -c && adb -s %s logcat -b events -v threadtime  > %s' % (device, device, logcatPath))
+	print 'runLogcat end. result=%d' % result
+
+def runMonkey(device, monkeyLogPath):
+	print 'runMonkey begin'
+	result = os.system('adb -s %s shell monkey -v -v -v --ignore-security-exceptions --ignore-crashes --ignore-native-crashes --ignore-timeouts  --throttle 200 --pkg-blacklist-file /sdcard/blacklist.txt 1000000 > %s' % (device, monkeyLogPath))
+	print 'runMonkey end. result=%d' % result
+	os._exit(0)
+
 def main(argv):
 	device = argv[1]
 	logcatPath = argv[2]
@@ -19,13 +32,19 @@ def main(argv):
 		print 'IOError'
 
 	begin = time.time()
-	os.system('adb -s %s logcat -b events -v threadtime  > %s &' % (device, logcatPath))
-	#adb shell monkey -v -v --ignore-crashes --ignore-native-crashes --ignore-timeouts  --throttle 200  100000
-	os.system('adb -s %s shell monkey -v -v -v --ignore-security-exceptions --ignore-crashes --ignore-native-crashes --ignore-timeouts  --throttle 200  --pkg-blacklist-file /sdcard/blacklist.txt 100000 > %s' % (device, monkeyLogPath))
-	#out = os.popen('jobs').read()
-	#print out
-	#outArray = re.split(r"\s+", out)
-	#os.system('kill %s' % outArray[2])
+	print 'begin %d' % begin
+	
+	t1 = threading.Thread(target=runLogcat, args=(device, logcatPath))
+	t2 = threading.Thread(target=runMonkey, args=(device, monkeyLogPath))
+
+	t2.start()
+	t1.start()
+	
+
 	end = time.time()
+	print 'end %d' % end
 	print 'duration: %d' % (end - begin)
+	#os._exit(0)
+
+
 main(sys.argv)

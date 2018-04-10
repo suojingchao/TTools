0f79e638190fd334390b7b1bdcff7d7252ca4d97
catsuo <catsuo@tencent.com>
2018-04-10 14:07:13 +0800

    fix some problems

--- keepAliveTest/monkeyAndLogcat.py
+++ keepAliveTest/monkeyAndLogcat.py
@@ -11,13 +11,13 @@ import threading
 
 def runLogcat(device, logcatPath):
 	print 'runLogcat begin'
-	result = os.system('adb -s %s logcat -c && adb -s %s logcat -b events -v threadtime  > %s' % (device, device, logcatPath))
-	print 'runLogcat end. result=%d' % result
+	result = os.system('adb -s %s logcat -c && adb -s %s logcat -b events -v threadtime  > %s &' % (device, device, logcatPath))
 
 def runMonkey(device, monkeyLogPath):
 	print 'runMonkey begin'
-	result = os.system('adb -s %s shell monkey -v -v -v --ignore-security-exceptions --ignore-crashes --ignore-native-crashes --ignore-timeouts  --throttle 200 --pkg-blacklist-file /sdcard/blacklist.txt 1000000 > %s' % (device, monkeyLogPath))
-	print 'runMonkey end. result=%d' % result
+	result = os.system('adb -s %s shell monkey -v -v -v --ignore-security-exceptions --ignore-crashes --ignore-native-crashes --ignore-timeouts  --throttle 200 --pkg-blacklist-file /sdcard/blacklist.txt 1000 > %s' % (device, monkeyLogPath))
+	end = time.time()
+	print 'end timestamp : %d' % end
 	os._exit(0)
 
 def main(argv):
@@ -32,19 +32,14 @@ def main(argv):
 		print 'IOError'
 
 	begin = time.time()
-	print 'begin %d' % begin
+	print 'begin timestamp : %d' % begin
 	
 	t1 = threading.Thread(target=runLogcat, args=(device, logcatPath))
 	t2 = threading.Thread(target=runMonkey, args=(device, monkeyLogPath))
 
-	t2.start()
 	t1.start()
+	t2.start()
+
 	
-
-	end = time.time()
-	print 'end %d' % end
-	print 'duration: %d' % (end - begin)
-	#os._exit(0)
-
-
-main(sys.argv)
+if __name__ == '__main__':
+	main(sys.argv)
